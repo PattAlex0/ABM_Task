26999e904e782d433531e3e31ad9308acdff4f1d
alxpttn <43339218+alxpttn@users.noreply.github.com>
2018-11-02 15:52:31 +0000

    Introduction of forLoops
    Changes:
    1.  Agent position and movement is not determined using forLoops.
    2. There are 10 agents, each of which moves 100 times
    3. Use of Torus to keep agents with boundaries

--- model.py
+++ model.py
@@ -11,68 +11,56 @@ import matplotlib.pyplot
 
 ##Values##
 agents = []
+numOfAgents = 10
+iterations = 100
 
-agents.append([randint(0, 99), randint(0, 99)])
-agents.append([randint(0, 99), randint(0, 99)])
+##Agent Movement##
+for i in range(numOfAgents):
+    #Assign 10 agents with x and y coordinates to agents list
+    agents.append([randint(0, 100), randint(0, 100)])
 
-print("Starting Positions: 0 =", agents[0][0], agents[0][1], ", 1 =", agents[1][0], agents[1][1])
+for i in range(iterations):
+    #Loop the next forloop for 100 iterations
+    for i in range(numOfAgents):
+        #Moves agent's y position randomly
+        if random() < 0.5:
+            agents[i][0] += 1
+        else:
+            agents[i][0] -= 1
 
-##Function - 0##
-#Moves agent 0 based on the outcome of the random function
-if random() < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
+        #If agent's y position is out of bounds, it is placed at the 'entrance' from the other boundary
+        if agents[i][0] <0:
+            agents[i][0] = 100
+        if agents [i][0] >100:
+            agents[i][0] = 0
 
-if random() < 0.5:
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
+        #Moves agent's x position randomly
+        if random() < 0.5:
+            agents[i][1] += 1
+        else:
+            agents[i][1] -= 1
 
-if random() < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
-
-if random() < 0.5:
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
-
-##Function - 1##
-#Moves agent 1 based on the outcome of the random function
-if random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-
-if random() < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
-
-if random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-
-if random() < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
-
-##Output##
-print("End Positions: 0 =", agents[0][0], agents[0][1], ", 1 =", agents[1][0], agents[1][1])
-
-#Calculates the distance between the two agents using Pythagoras' theorem
-answer = ((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2)**0.5
-print("Distance =", answer)
+        #If agent's x position is out of bounds, it is placed at the 'entrance' from the other boundary
+        if agents[i][1] <0:
+            agents[i][1] = 100
+        if agents [i][1] >100:
+            agents[i][1] = 0
 
 ##Graph##
-matplotlib.pyplot.ylim(0, 99)
-matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
-matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
-m = max(agents, key = itemgetter(1)) #selects point furthest east
-matplotlib.pyplot.scatter(m[1], m[0], color='red') #Colours the point furthest east red
+#Set x and y limits for graph
+matplotlib.pyplot.ylim(0, 100)
+matplotlib.pyplot.xlim(0, 100)
+
+#Plots each agent on the graph using a forloop
+for i in range(numOfAgents):
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+
+#Colours the point furthest east black  
+m = max(agents, key = itemgetter(1))
+matplotlib.pyplot.scatter(m[1], m[0], color='black')
 matplotlib.pyplot.show()
+
+
+##Currently not in use##
+#Calculates the distance between the two agents using Pythagoras' theorem
+#answer = ((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2)**0.5
