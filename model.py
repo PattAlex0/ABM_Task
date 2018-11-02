d82b89c7f667d24b31aab6972ce22c23ae34feb3
alxpttn <43339218+alxpttn@users.noreply.github.com>
2018-11-02 13:28:17 +0000

    Added List and Graphics
    Changes:
    1. Agents are now held from within a list
    2. Agent positions are now plotted using the matplotlib library

--- model.py
+++ model.py
@@ -3,63 +3,76 @@ from random import randint, random
     #randint can be used to produce a random integer
     #random can be used to produce a random number between 0 and 1
 
+from operator import itemgetter
+    #itemgetter can be used to select specific elements
+
+import matplotlib.pyplot
+    #Can be used to create graphs
+
 ##Values##
-#Creates x and y coordinates (between 0 and 99) for two agents
-x0 = randint(0, 99)
-y0 = randint(0, 99)
+agents = []
 
-x1 = randint(0, 99)
-y1 = randint(0, 99)
+agents.append([randint(0, 99), randint(0, 99)])
+agents.append([randint(0, 99), randint(0, 99)])
 
-print("Starting Positions: 0 =", x0, y0, ", 1 =", x1, y1)
+print("Starting Positions: 0 =", agents[0][0], agents[0][1], ", 1 =", agents[1][0], agents[1][1])
 
 ##Function - 0##
 #Moves agent 0 based on the outcome of the random function
 if random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
 
 if random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
+    agents[0][1] -= 1
 
 if random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
 
 if random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
+    agents[0][1] -= 1
 
 ##Function - 1##
 #Moves agent 1 based on the outcome of the random function
 if random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
 
 if random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
 
 if random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
 
 if random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
 
 ##Output##
-print("End Positions: 0 =", x0, y0, ", 1 =", x1, y1)
+print("End Positions: 0 =", agents[0][0], agents[0][1], ", 1 =", agents[1][0], agents[1][1])
 
 #Calculates the distance between the two agents using Pythagoras' theorem
-answer = ((y0 - y1)**2) + ((x0 - x1)**2)**0.5
+answer = ((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2)**0.5
 print("Distance =", answer)
+
+##Graph##
+matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
+matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
+m = max(agents, key = itemgetter(1)) #selects point furthest east
+matplotlib.pyplot.scatter(m[1], m[0], color='red') #Colours the point furthest east red
+matplotlib.pyplot.show()
