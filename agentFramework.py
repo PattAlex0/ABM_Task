f55bbfda6d016d92270da4f6e67b62dbf078e700
alxpttn <43339218+alxpttn@users.noreply.github.com>
2018-12-17 12:06:42 +0000

    Final Presentation of Model

--- agentFramework.py
+++ agentFramework.py
@@ -1,7 +1,23 @@
-## Modules ##
+# agentFramework.py
+
+"""
+Created by Alex Patterson (GitHub @ alxpttn)
+
+Creates the agent class, including its x and y features and behavioural functions
+
+Requires random module
+"""
+
+# Instructions:
+# To modify the possible x and y coordinates for all agents, change the randint range for self._x and self._y
+# To modify the probabilty of movement, change the probability value in the move function
+# To modify how much data an agent takes from the environment, change the store value in the eat function
+# To modify how much data an agent steals from another agent, change the store values in the stealFromNeighbours function
+# To modify how much data an agent must have before expunging, change the values in the vomit function
+
 from random import random as rand, randint
 
-## Classes ##
+#Agent Class
 class agent():
     """
     An agent for use in the model
@@ -31,7 +47,6 @@ class agent():
         #Connect to global distance list
         self.agentsDistance = agentsDistance
 
-
     def getx(self):
         return self._x
     def gety(self):
@@ -47,10 +62,11 @@ class agent():
     def dely(self):
         del self._y
 
+    #Define x and y positions as properties of the agents
     x = property(getx, setx, delx, 'The x coordinate of an agent.')
     y = property(gety, sety, dely, 'The y coordinate of an agent.')
 
-    #Functions
+    #Move function
     def move(self):
         """
         Moves agent in a random direction
@@ -67,11 +83,14 @@ class agent():
             self.y = (self.y + 1) % 299
         else:
             self.y = (self.y - 1) % 299
-    
+
+    #Eat function
     def eat(self):
         """
         Allows agents to 'eat' the environment
         """
+
+        #Determine if there is enough data to consume
         if self.environment[self.y][self.x] > 10:
             
             #Remove environment data
@@ -80,17 +99,30 @@ class agent():
             #Add data to agent's own store
             self.store += 10
 
+        #Determine if there is any 'leftover' data
+        elif self.environment[self.y][self.x] > 0:
+
+            #Add leftover data to agent's store
+            self.store += self.environment[self.y][self.x]
+
+            #Remove all data from environment
+            self.environment[self.y][self.x] = 0
+            
+
+    #Share function
     def shareWithNeighbours(self, neighbourhood):
         """
         Allows agents to share data with each other
         """
+
+        #Create loop to assess all possible agents
         for agent in self.agents:
 
             #Calculate distance between other agents
             distance = self.distanceBetween(agent)
 
-            #Threshold checker
-            if distance <= neighbourhood:
+            #Check if any agent is close enough and if they have equal or more data
+            if distance <= neighbourhood and agent.store >= self.store:
 
                #Add together data from both agents
                distSum = self.store + agent.store
@@ -102,46 +134,49 @@ class agent():
                self.store = distAvg
                agent.store = distAvg
 
+    #Steal function
+    def stealFromNeighbours(self, neighbourhood):
+       """
+       Allows agents to steal data from other agents if the are low on food
+       """
+        
+       #Create loop to assess all possible agents
+       for agent in self.agents:
+
+          #Calculate distance between other agents
+          distance = self.distanceBetween(agent)
+
+          #Check if any agent is close enough, if the current agent is low on data,
+          #and if the other agent has a minium amount of data
+          if distance <= neighbourhood and self.store <= 10 and agent.store >= 10:
+
+              #Add 10 to the thief's store
+              self.store += 10
+
+              #Remove 10 from the agent who was robbed
+              agent.store -= 10
+
+    #Vomit function
     def vomit(self):
         """
         Forces agents to expunge data if they contain too much
         """
 
+        #Check if agent has too much data
         if self.store > 100:
             
+            #Remove 100 from agent's store
             self.store -= 100
             
-            self.environment[self.y][self.x] += 100
+            #Add 100 back into the environment
+            #self.environment[self.y][self.x] += 100
 
+    #Distance function
     def distanceBetween(self1, self2):
         """
         Calculates the distance between agents
         """
-        
+
+        #Calculate distance between agents using the pythygoras theorem
         answer = ((self1.x - self2.x)**2) + ((self1.y - self2.y)**2)**0.5
         return answer
-
-    def distanceCalculator(self):
-        '''
-        Calculates the distance between each agent using the distanceBetween function
-        Repeated agents are skipped over by adding them to the agentsDum list, which is checked in the loop
-        '''
-
-        #Create dummy list
-        agentsDum = []
-
-        #Append each agent to dummy list
-        agentsDum.append(self)
-
-        for agent in self.agents:
-
-            #Check if agent has already been looked at
-            if agent in agentsDum:
-                continue
-
-            #Append each distance value to the global distance list
-            else:
-                self.agentsDistance.append(self.distanceBetween(agent))
-
-
-        
--- model.py
+++ model.py
@@ -1,36 +1,50 @@
-## Modules ##
+# model.py
 
-#Agent Class
+"""
+Created by Alex Patterson (GitHub @ alxpttn)
+
+Runs an Agent Based model for a predetermined number of iterations
+
+Requires agentFramework.py and in.txt, as well as csv, matplotlib, and random modules
+
+"""
+
+# Instructions:
+# Ensure that agentFramework.py and in.txt are in the same directory as model.py
+# After running, two txt files should be given in same directory
+# outE.txt saves the changed environment
+# outA.txt saves how much data each agent collected
+
+# Modules
 import agentFramework as AF
-
-#Environment and Graphics
 import csv
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
+from random import random         
 
-#Other
-from random import shuffle, random         
-from operator import itemgetter
+# Environment List - Holds values for the environment
+environment = []
 
-## Values ##
+# Agents List - Holds values for each agent
+agents = []
 
-#Agent Lists
-environment = []        #Holds values for the environment
-agents = []             #Holds values for each agent
-agentsDistance = []     #Holds distances between agents
+# Distance List - Holds distances between agents
+agentsDistance = []     
 
-#Iterables
-numOfAgents = 50        #Number of agents
-iterations = 100        #Number of times the movement loop is run
-neighbourhood = 20      #Value which determines how close agents have to be to one another to be considered in the same location
+# Number of Agents - How many agents are in the model
+numOfAgents = 50
 
-##  Environment  ##
+# Iterations - Number of times the behavioural loop is run
+iterations = 100
 
-#Import raster data
-f = open('in.txt', newline = '')
-reader = csv.reader(f, quoting = csv.QUOTE_NONNUMERIC)
+# Neighbourhood - How close agents have to be to one another to be considered in the same location
+neighbourhood = 20
 
-#Add raster data to environment list
+# Import raster data for use as environment
+input = open('in.txt', newline = '')
+reader = csv.reader(input, quoting = csv.QUOTE_NONNUMERIC)
+
+# Add raster data to environment list
 for row in reader:
     '''
     Copies the csv data into the environment list
@@ -43,66 +57,79 @@ for row in reader:
 
     environment.append(rowlist)
 
-f.close()
+input.close()
 
-#Create plot object for later use
+# Create plot object for later use
 fig = plt.figure(figsize = (7, 7))
 
 ax = fig.add_axes([0, 0, 1, 1])
 #ax.set_autoscale_on(False)
-
-## Agent Movement ##
 
 # Assign initial positions for each agent
 for i in range(numOfAgents):
     
     agents.append(AF.agent(environment, agents, agentsDistance))
 
-#Create animation and movement loop
+# Create animation and behaviour loop
 def update(frame_number):
     '''
-    Movement direction is determined by use of random() function
-    Modulus operater is used to divide each number by 299
-    If any number goes to 299, it will have 0 remainders and so be assigned a 0 coordinate
+    Basis of animation
+    Refreshes the environment graphic, enacts agent behaviours, and plots agents in new positions
     '''
 
-    #Update plot per loop
+    # Update plot per loop
     fig.clear()
     plt.ylim(0, 299)
     plt.xlim(0, 299)
     plt.imshow(environment)
         
-    #Agent behaviour per loop
+    # Agent behaviour per loop
     for i in range(numOfAgents):
         agents[i].move()
         agents[i].eat()
         agents[i].vomit()
         agents[i].shareWithNeighbours(neighbourhood)
+        agents[i].stealFromNeighbours(neighbourhood)
 
-    #Plot agent position per loop
+    # Plot agent position per loop
     for i in range(numOfAgents):
         plt.scatter(agents[i].x, agents[i].y)
-        #print(agents[i].x, agents[i].y)
 
-#Generator Function
+# Generator Function
 def genFunction(b = [0]):
     """
     Genates animaion for a predetermined number of steps
     """
+
+    # Assign starting value for counter
     a = 0
-    while (a < 100):
+    
+    # Check if counter has surpassed number of given iterations
+    while (a < iterations):
+
+        # Add to counter each time the animation is run
         yield a
         a += 1
 
-#Movement Animation
+# Create animations
 animation = anim.FuncAnimation(fig, update, repeat=False, frames = genFunction)
 plt.show()
-      
-## Distance Loop ##
-shuffle(agents)
+
+# Save the modified environment to a txt file
+outputE = open('outE.txt', 'w')
+outputE.write(str(environment))
+outputE.close()
+
+# Save report of collected data by agents to
+# a txt file
+outputA = open('outA.txt', 'w', newline = '')
+AResults = []
 
 for i in range(numOfAgents):
-    agents[i].distanceCalculator()
+    AResults.append('Agent ' + str(i + 1) + ' collected ' + str(agents[i].store) + ' bits of data')
 
-print("Maximum distance = " + str(max(agents[i].agentsDistance)) +   #Prints the maximum and minimum distances
-           ", Minimum distance = " + str(min(agents[i].agentsDistance)))
+outputA.write(str(AResults))
+outputA.close()
+
+# Check full program ran successfully
+print('Program successfully run.')
